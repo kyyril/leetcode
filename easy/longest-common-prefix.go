0393ceb2f46a9a9e1fc8836e10aaf3485ceea849
kyyril <khairilrahman55@gmail.com>
2025-07-06 01:43:49 +0700

    Add LeetCode problem 14: Longest Common Prefix solution in Go

---
+++ easy/longest-common-prefix.go
@@ -0,0 +1,22 @@
+package main
+
+import "strings"
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	prefix := strs[0]
+
+	for i := 1; i < len(strs); i++ {
+		for len(prefix) > 0 && !strings.HasPrefix(strs[i], prefix) {
+			prefix = prefix[:len(prefix)-1]
+		}
+		if prefix == "" {
+			return ""
+		}
+	}
+
+	return prefix
+}
