1d0da699b155c65f1c97da073631ab0353d25046
kyyril <khairilrahman55@gmail.com>
2025-09-02 16:33:13 +0700

    feat(medium): Add solution for addTwoNumbers to sum two linked lists

---
+++ medium/addTwoNumbers.ts
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(
+  l1: ListNode | null,
+  l2: ListNode | null
+): ListNode | null {
+  let dummyHead = new ListNode(0); // node awal dummy
+  let current = dummyHead;
+  let carry = 0;
+
+  while (l1 !== null || l2 !== null || carry > 0) {
+    let sum = carry;
+
+    if (l1 !== null) {
+      sum += l1.val;
+      l1 = l1.next;
+    }
+    if (l2 !== null) {
+      sum += l2.val;
+      l2 = l2.next;
+    }
+
+    carry = Math.floor(sum / 10); // simpan carry
+    current.next = new ListNode(sum % 10); // buat node baru
+    current = current.next;
+  }
+
+  return dummyHead.next; // hasil mulai dari next dummy
+}
